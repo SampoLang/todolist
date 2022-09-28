2edf9ec0aaf78f65db509745494a5593666be04d
SampoLang <bgu444@myy.haaga-helia.fi>
2022-09-28 19:53:34 +0300

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function Todolist() {
+  const [todo, setTodo] = useState({
+    description: "",
+    date: ""
+  });
+  const [todos, setTodos] = useState([]);
+
+  const descChanged = (event) => {
+    setTodo({ ...todo, description: event.target.value });
+  }
+  const dateChanged = (event) => {
+    setTodo({ ...todo, date: event.target.value });
+  }
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([todo, ...todos]);
+    setTodo({ description: "", date: "" });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Todolist</h1>
+      <fieldset><legend>Add Todo:</legend>
+        <form onSubmit={addTodo}>
+          <label for="desc"> Description: </label>
+          <input name="desc" type="text" value={todo.description} onChange={descChanged}></input>
+          <label for="date"> Date: </label>
+          <input name="date" type="text" value={todo.date} onChange={dateChanged}></input>
+          <input type="submit" value="Add"></input>
+
+        </form>
+      </fieldset>
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+        </tr>
+        <tbody>
+          {
+            todos.map((todo, index) =>
+              <tr key={index}>
+                <td>{todo.date}</td>
+                <td>{todo.description}</td>
+              </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+export default Todolist;
